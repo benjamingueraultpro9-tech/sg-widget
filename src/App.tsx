72aefdc1ce974c886ca000361b90f17f929744aa
benjaming <benjaming@MACOSX170.local>
2026-07-13 16:19:40 +0200

    fix pixelDensity + nouvelle animation orange

--- src/App.tsx
+++ src/App.tsx
@@ -2,31 +2,36 @@ import { ShaderGradientCanvas, ShaderGradient } from '@shadergradient/react'
 
 export default function App() {
   return (
-    <ShaderGradientCanvas style={{ position: 'absolute', inset: 0, width: '100%', height: '100%' }}>
+    <ShaderGradientCanvas
+      style={{ position: 'absolute', inset: 0, width: '100%', height: '100%' }}
+      pixelDensity={1.8}
+    >
       <ShaderGradient
         animate="on"
-        brightness={1}
+        brightness={1.2}
         cAzimuthAngle={180}
-        cDistance={2.81}
-        cPolarAngle={80}
-        cameraZoom={9.1}
-        color1="#606080"
-        color2="#8d7dca"
-        color3="#212121"
+        cDistance={3.6}
+        cPolarAngle={90}
+        cameraZoom={1}
+        color1="#ff5005"
+        color2="#dbba95"
+        color3="#d0bce1"
         envPreset="city"
         grain="on"
         lightType="3d"
-        positionX={0}
+        positionX={-1.4}
         positionY={0}
         positionZ={0}
         reflection={0.1}
-        rotationX={50}
-        rotationY={0}
-        rotationZ={-60}
+        rotationX={0}
+        rotationY={10}
+        rotationZ={50}
         type="waterPlane"
-        uDensity={1.5}
-        uSpeed={0.3}
-        uStrength={1.5}
+        uAmplitude={1}
+        uDensity={0.7}
+        uFrequency={5.5}
+        uSpeed={0.4}
+        uStrength={2.7}
         wireframe={false}
       />
     </ShaderGradientCanvas>
